7a66f728bde44f76e970329fcec6be98f0d24b31
Ivan Montiel <idmontie@asu.edu>
2015-02-08 14:32:12 -0700

    initial scaffolding.

---
+++ scripts/font-scraper.js
@@ -0,0 +1,39 @@
+// =====================
+// Google Fonts Scraper
+//
+// Ivan Montiel
+// Feb 8, 2015
+// =====================
+
+// Pretty sure this violates some sort of terms
+// of service EULA.
+
+// Get the font elements
+var fels = document.getElementsByClassName( 'fontcard' );
+var fontNames = [];
+
+for ( var i = 0; i < fels.length; i++ ) {
+  // Get the fontname elements
+  // There should only be one object in fnels.
+  var fnels = fels[i].getElementsByClassName( 'fontname' );
+  var sels  = fnels[0].getElementsByTagName( 'span' );
+  var fontName = sels[0].textContent;
+
+
+  // Get the preview element
+  var pels = fels[i].getElementsByClassName( 'preview' );
+
+  // Get the font slug based on the class name
+  // Should be good for now, but will probably
+  // change in the future (i.e. if this script
+  // stops working, this is why)
+  var fontSlug = pels[0].classList[1];
+  fontNames.push( {
+    name : fontName,
+    slug: fontSlug 
+  } );
+}
+
+// TODO some how scrape 
+
+console.log( JSON.stringify( fontNames ) );
